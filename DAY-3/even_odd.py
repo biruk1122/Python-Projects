22923f66bbb517f29a52c286db329732db15e729
biruk1122 <birukfikadu29@gmail.com>
2024-03-30 12:17:30 -0700

    even odd

---
+++ DAY-3/even_odd.py
@@ -0,0 +1,8 @@
+"""To Find Out a Number is Even or Odd"""
+
+print("Enter Even or Odd Number")
+number = int(input("Enter Your Number "))
+if number % 2 == 0:
+    print("Your Number is Even Number")
+else:
+    print("Your Number is Odd Number")
