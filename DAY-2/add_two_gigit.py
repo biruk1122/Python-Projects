cd75db72d0ea3e28874bd682105257e9567c54f9
biruk1122 <birukfikadu29@gmail.com>
2024-03-30 08:15:32 -0700

    digit number

---
+++ DAY-2/add_two_gigit.py
@@ -0,0 +1,7 @@
+"""This Program adds the twp digit numbers. Example, if the input number is 35 the output will be 3 + 5 = 8"""
+
+number = input("Enter Two Digit Number\n")
+first_digit = int(number[0])
+second_digit = int(number[1])
+two_digit_number = first_digit + second_digit
+print(two_digit_number)
