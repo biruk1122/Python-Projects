5ab5a4fdd18f4d8bbd4fad28b2a98fa947f5424c
biruk1122 <birukfikadu29@gmail.com>
2024-03-30 09:02:52 -0700

    weeks

---
+++ DAY-2/weeks_left.py
@@ -0,0 +1,7 @@
+"""Write A Program using a maths and f-string that tells as how many weeks we have left if we have lived until 90
+years old."""
+
+age = input("Enter Your Age\n")
+years = 90 - int(age)
+weeks = years * 52
+print(f"You Have {weeks} Left")
