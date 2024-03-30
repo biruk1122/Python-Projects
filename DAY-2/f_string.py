9b02c8f6108bfded751161d86a7d12894d0d2c1b
biruk1122 <birukfikadu29@gmail.com>
2024-03-30 08:51:14 -0700

    f-string

---
+++ DAY-2/f_string.py
@@ -0,0 +1,6 @@
+"""f-String used to print out String, int, Float at the same Time"""
+
+score = 0
+height = 1.7
+iscorrect = True
+print(f"Your Score is {score}, Your Height is {height}, Your Result is {iscorrect}")
