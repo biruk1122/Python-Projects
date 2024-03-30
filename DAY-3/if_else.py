23c75bb5ff7c01bb3e0af2af1fd72006dc6ed9f2
biruk1122 <birukfikadu29@gmail.com>
2024-03-30 12:06:51 -0700

    if elase

---
+++ DAY-3/if_else.py
@@ -0,0 +1,9 @@
+"""if else conditions"""
+
+print("Welcome to The Rollercoaster!")
+height = int(input("What is Your Height?\n"))
+
+if height > 120:
+    print("You Can Ride the Rollercoast")
+else:
+    print("Sorry, You Have to Grow Taller Before You Can Ride")
