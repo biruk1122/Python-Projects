3313ba9693b00aa6c9519ef9d06355a318472743
biruk1122 <birukfikadu29@gmail.com>
2024-04-04 08:03:05 -0700

    updated

---
+++ DAY-4/lists.py
@@ -0,0 +1,5 @@
+fruits = ["Bannana", "Orange", "Mango", "Apples", "Grapes", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+punch = [fruits, vegetables]
+print(punch)
