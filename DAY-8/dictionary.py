e2ca02acffe772957b0851e6417d1c4cc482d871
biruk1122 <birukfikadu29@gmail.com>
2024-04-06 08:09:08 -0700

    dictionary

---
+++ DAY-8/dictionary.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevent the program from running as expected.", 
+    "Function": "A piece of code you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again"
+}
+# Retriving Items From Dictionary
+print(programming_dictionary["Bug"])
+
+# Adding Items to Dictionary
+programming_dictionary["Film"] = "Every body waches to intertain them selve"
+print(programming_dictionary)
