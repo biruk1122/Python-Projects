6c1e75b51864dd0e6aa7c284b4345723762bb7e7
biruk1122 <birukfikadu29@gmail.com>
2024-04-10 02:40:42 -0700

    updated

---
+++ DAY-14/turtle.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+print(timmy)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
