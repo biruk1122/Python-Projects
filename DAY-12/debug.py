816cfaf9bbd39cf90732562a7a91a5238feed3f6
biruk1122 <birukfikadu29@gmail.com>
2024-04-10 02:11:40 -0700

    updated

---
+++ DAY-12/debug.py
@@ -0,0 +1,6 @@
+pages = 0
+word_per_page = 0
+pages = int(input("Number of Pages: "))
+word_per_page = int(input("Number of Words Per Page: "))
+total_words = pages * word_per_page
+print(total_words)
