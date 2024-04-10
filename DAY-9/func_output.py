0cc69bc135fc58487ccd48c2eaa60c77b4fa2aa6
biruk1122 <birukfikadu29@gmail.com>
2024-04-10 00:19:05 -0700

    updated

---
+++ DAY-9/func_output.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
+print(format_name(input("What is Your First Name "), input("What is Your Last Name")))
