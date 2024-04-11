bff354f432cb1dcbd1550648038e7d4a3a1cfd6c
biruk1122 <birukfikadu29@gmail.com>
2024-04-11 06:12:39 -0700

    updated

--- DAY-13/coffee_machine.py
+++ DAY-13/coffee_machine.py
@@ -83,3 +83,4 @@ while is_on:
            payment = process_coins()
            if is_transaction_successful(payment, drink["cost"]):
                make_coffee(choice, drink["ingredients"])
+               
