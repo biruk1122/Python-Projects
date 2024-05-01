95d495a2135ff81654fc126f20ca54c6219f7c0b
biruk1122 <birukfikadu29@gmail.com>
2024-05-01 01:16:50 -0700

    updated

--- 024-DAY/tkinter.py
+++ 024-DAY/tkinter.py
@@ -1,10 +1,33 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("GUI Interface")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="This is Label", font=("Arial", 24, "bold"))
+# Label
+
+my_label = Label(text="This is Label", font=("Arial", 24, "bold"))
 my_label.pack(expand=True)
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+
+
+def button_clicked():
+    print("I got clicked")
+    input_text = entry.get()
+    my_label.config(text=input_text)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+
+entry = Entry(width=10)
+entry.pack()
+print(entry.get())
 
 window.mainloop()
