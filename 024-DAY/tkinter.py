79843d77cb4cd0fd1d77f133a56e91aad0795694
biruk1122 <birukfikadu29@gmail.com>
2024-05-01 01:55:55 -0700

    updated

--- 024-DAY/tkinter.py
+++ 024-DAY/tkinter.py
@@ -3,11 +3,13 @@ from tkinter import *
 window = Tk()
 window.title("GUI Interface")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 # Label
 
 my_label = Label(text="This is Label", font=("Arial", 24, "bold"))
-my_label.pack(expand=True)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text")
@@ -19,15 +21,19 @@ def button_clicked():
     print("I got clicked")
     input_text = entry.get()
     my_label.config(text=input_text)
+    my_label.grid(column=0, row=0)
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
 
 # Entry
 
 entry = Entry(width=10)
-entry.pack()
 print(entry.get())
+entry.grid(column=2, row=2)
 
 window.mainloop()
